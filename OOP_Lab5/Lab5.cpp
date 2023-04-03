19ca7c47ed4f769d9a4d1090e04dab26c94a0868
Olexandr Molibozhko <74785462+DecodeFF@users.noreply.github.com>
2023-04-03 23:23:18 +0300

    Update Lab5.cpp

--- OOP_Lab5/Lab5.cpp
+++ OOP_Lab5/Lab5.cpp
@@ -2,24 +2,18 @@
 #include <fstream>
 using namespace std;
 
-const int MAX_SIZE = 122; // maximum size of array
+const int MAX_SIZE = 122; 
 
 int main()
 {
-   ifstream inFile("text2.txt"); // open the input file
-   double arr[MAX_SIZE];           // create a 1D array to store numbers
+   ifstream inFile("text2.txt"); 
+   double arr[MAX_SIZE];  
    int i = 0;
    while (inFile >> arr[i] >> arr[i + 1] && i < MAX_SIZE)
-   { // read numbers from file and store in array
+   { 
       i += 2;
    }
-   inFile.close(); // close the input file
-
-   // print the numbers in the array
-   // for (int j = 0; j < i; j += 2)
-   // {
-   //    cout << arr[j] << " " << arr[j + 1] << endl;
-   // }
+   inFile.close(); 
    double t, Min = 0, Max = 0, iMin = 0, iMax = 0;
    for (int i = 1; i <= 122; i += 2)
    {
